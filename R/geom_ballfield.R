8efdad2c3a0666344effa09e3b60d3a9cd1bd2ad
‘evanmascitti’ <evanmascitti@gmail.com>
2020-11-03 13:07:40 -0500

    adjusted location of control points for outfield arc and foul lines in geom_ballfield

--- R/geom_ballfield.R
+++ R/geom_ballfield.R
@@ -9,12 +9,13 @@
 #' @export
 #'
 geom_ballfield <- function() {
-  list(
-    ggplot2::geom_curve(x = 30, xend = 223, y = -95, yend = -90, curvature = -0.50), # OF arc
-    ggplot2::geom_segment(x = 128,  xend = 30, y = -208, yend = -95),  #LF line
-    ggplot2::geom_segment(x = 128, xend = 223, y = -208, yend = -90),  # RF line
+    list(
+    ggplot2::geom_curve(x = 34, xend = 220, y = -98, yend = -98, curvature = -0.65), # OF arc
+    ggplot2::geom_segment(x = 128,  xend = 34, y = -208, yend = -98),  #LF line
+    ggplot2::geom_segment(x = 128, xend = 220, y = -208, yend = -98),  # RF line
     ggplot2::geom_curve(x = 81, xend = 172, y = -154, yend = -154, curvature = -0.65), # inf arc
     ggplot2::geom_segment(x = 101,  xend = 127, y = -176, yend = -150),  #2b-3b line
     ggplot2::geom_segment(x = 155,  xend = 127, y = -176, yend = -150)  #1b-2b line
-    )
+  )
 }
+
